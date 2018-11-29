382a5620bf30b7ad0ea66acdbc04b773cfae4808
Sidi Mustaqbal <sidi.mustaqbal@gmail.com>
2018-11-29 17:40:41 +0700

    [Core] Set "start-bootstrap" as default frontend theme

--- application/core/Public_Controller.php
+++ application/core/Public_Controller.php
@@ -6,5 +6,12 @@ class Public_Controller extends MY_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+
+		$this->template->set_theme('start-bootstrap');
+
+		$this->template->set_partial('navigation','partials/navigation');
+		$this->template->set_partial('metadata','partials/metadata');
+
+		$this->load->helper('admin_theme');
 	}
 }
