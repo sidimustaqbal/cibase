d07e55b09a1a2ceec69a82d168185be39d7a82fa
Sidi Mustaqbal <sidi.mustaqbal@gmail.com>
2018-12-02 19:11:28 +0700

    [Landing Page] Change button if user logged in

--- application/themes/start-bootstrap/views/partials/navigation.php
+++ application/themes/start-bootstrap/views/partials/navigation.php
@@ -1,7 +1,15 @@
 <!-- Navigation -->
 <nav class="navbar navbar-light bg-light static-top">
 	<div class="container">
-		<a class="navbar-brand" href="#">Start Bootstrap</a>
-		<a class="btn btn-primary" href="#">Sign In</a>
+		<a class="navbar-brand" href="<?php echo site_url(); ?>"><?php echo get_settings('site-title'); ?></a>
+
+		<div class="menu-buttons">
+		<?php if(isset($this->current_user)) { ?>
+			<a class="btn btn-primary" href="<?php echo site_url('admin'); ?>">Admin Panel</a>
+			<a class="btn btn-primary" href="<?php echo site_url('users/auth/logout'); ?>">Logout</a>
+		<?php } else { ?>
+			<a class="btn btn-primary" href="<?php echo site_url('users/auth/login'); ?>">Login</a>
+		<?php } ?>
+		</div>
 	</div>
 </nav>
