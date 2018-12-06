7305006d284c93761af2bfc45ada7d0024d8e6f7
Sidi Mustaqbal <sidi.mustaqbal@gmail.com>
2018-12-06 10:12:15 +0700

    [settings] add indonesian language

--- application/modules/settings/language/indonesian/settings_lang.php
+++ application/modules/settings/language/indonesian/settings_lang.php
@@ -1,3 +1,31 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['settings:nav_settings'] = 'Pengaturan';
+
+// Pengaturan
+$lang['settings:settings:plural'] = 'Pengaturan';
+$lang['settings:settings:singular'] = 'Pengaturan';
+$lang['settings:settings:create'] = 'Tambah Pengaturan';
+$lang['settings:settings:edit'] = 'Ubah Pengaturan';
+$lang['settings:settings:view'] = 'Tinjau Pengaturan';
+$lang['settings:settings:create_success'] = 'Pengaturan berhasil dibuat';
+$lang['settings:settings:create_failed'] = 'Gagal membuat Pengaturan';
+$lang['settings:settings:edit_success'] = 'Pengaturan berhasil diubah';
+$lang['settings:settings:edit_failed'] = 'Gagal mengubah Pengaturan';
+$lang['settings:settings:delete_success'] = 'Pengaturan berhasil dihapus';
+$lang['settings:settings:delete_failed'] = 'Gagal menghapus Pengaturan';
+
+$lang['settings:title'] = 'Judul';
+$lang['settings:slug'] = 'Slug';
+$lang['settings:description'] = 'Deskripsi';
+$lang['settings:default'] = 'Default';
+$lang['settings:value'] = 'Nilai';
+$lang['settings:type'] = 'Tipe';
+$lang['settings:type_not_found'] = 'Tipe tidak ditemukan';
+$lang['settings:options'] = 'Pilihan';
+$lang['settings:options_format_wrong'] = 'Format Pilihan salah';
+$lang['settings:group'] = 'Grup Pengaturan';
+$lang['settings:is_required'] = 'Wajib di isi';
+$lang['settings:is_required:0'] = 'Tidak';
+$lang['settings:is_required:1'] = 'Ya';
+$lang['settings:order'] = 'Urutan';
